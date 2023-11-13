9eaf220ca36f68c9423a606ac946d78939f19020
Piotrek <Swistekxd@gmail.com>
2023-11-13 05:24:16 +0100

    inprompt final

--- src/controllers/inprompt.ts
+++ src/controllers/inprompt.ts
@@ -14,6 +14,18 @@ type TaskData = {
     input: string[];
     question: string;
 };
+
+const filterByName = (messages: string[], name: string): string[] => {
+    const lowerCaseName = name.toLowerCase();
+    return messages.filter(message =>
+        message.toLowerCase().includes(lowerCaseName),
+    );
+};
+const getLastWord = (sentence: string): string => {
+    const words = sentence.trim().split(" ");
+    return words[words.length - 1].replace(/[?!.]/g, "");
+};
+
 export const inprompt = async (req: Request, res: Response): Promise<void> => {
     const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY,
@@ -21,15 +33,22 @@ export const inprompt = async (req: Request, res: Response): Promise<void> => {
     try {
         const data = await Authorization("inprompt");
         const { input, question }: TaskData = await getTaskData(data.token);
+        const name = getLastWord(question);
 
+        const answer = filterByName(input, name)[0];
 
-        res.json({ input, question }).status(200);
+        const sendAnswer2 = await sendAnswer({
+            token: data.token,
+            answer,
+        });
+
+        res.json({ sendAnswer2 }).status(200);
     } catch (error) {
         console.error(error);
         res.json({
             status: 500,
             message: "Internal Server Error",
-            error: error.message,
+            error: error,
         }).status(500);
     }
 };
